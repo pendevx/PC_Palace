9f286c3d17df7b34cd702d932c4163d33b350e8e
pendevx <pendevx@gmail.com>
2024-03-22 00:34:19 +1300

    commit

--- configurations/firebase.js
+++ configurations/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_APIKEY,
-  authDomain: "pc-palace-firebase.firebaseapp.com",
-  projectId: "pc-palace-firebase",
-  storageBucket: "pc-palace-firebase.appspot.com",
-  messagingSenderId: "36614120009",
-  appId: "1:36614120009:web:0bfb5174cbacf1ad459598",
-  measurementId: "G-DFFVS9MX4V"
+  authDomain: "pc-palace.firebaseapp.com",
+  projectId: "pc-palace",
+  storageBucket: "pc-palace.appspot.com",
+  messagingSenderId: "917374258076",
+  appId: "1:36614120009:web:0bfb5174cbacf1ad459598", //
+  measurementId: "G-DFFVS9MX4V" //
 };
 
 // Initialize Firebase
